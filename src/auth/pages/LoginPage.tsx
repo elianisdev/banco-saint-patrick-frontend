672ad8d5b898c0a7ec427da012a4f4c3249e5e95
Eliana Suancha Guzman <91162334+elianisdev@users.noreply.github.com>
2026-01-13 09:51:13 -0500

    rama dev (#2)

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 export const LoginPage = () => {
     return (
         <div className="p-4">
-            <h1 className="text-xl font-bold">Login</h1>
+            <h1 className="text-xl font-bold">Login1</h1>
         </div>
     )
 }
