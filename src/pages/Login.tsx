adc291597049123ab0dc6f5a385fea71e6014b6e
JeremySoto1 <108085932+JeremySoto1@users.noreply.github.com>
2026-03-09 15:52:27 -0500

    Feature/ingresar (#18)
    * feature/login
    
    * Agrega pantalla de login y verificación
    
    * Ventana de Alias y mejora de Login
    
    * Pantalla de Ingresar y añadido el TOAST de verificación
    
    ---------
    
    Co-authored-by: Eliana Suancha Guzman <91162334+elianisdev@users.noreply.github.com>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,16 +22,6 @@ export const Login = () => {
       <div className="flex flex-col items-center justify-center flex-grow">
 
         {/* Logo */}
-
-export const Login = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="min-h-screen flex flex-col bg-white">
-      
-      {/* Contenido centrado */}
-      <div className="flex flex-col items-center justify-center flex-grow">
-        
-        {/* Logo y título */}
         <div className="flex flex-col items-center mb-6">
           <img src={logo} alt="Banco Saint Patrick" className="w-14 mb-2" />
           <h1 className="text-lg font-semibold text-gray-800">
@@ -47,8 +37,6 @@ export const Login = () => {
           onSubmit={handleSubmit(onSubmit)}
           className="w-full max-w-sm bg-[#fbfde9] border border-gray-300 rounded-lg shadow-md p-6"
         >
-        {/* Card del login */}
-        <div className="w-full max-w-sm bg-[#fbfde9] border border-gray-300 rounded-lg shadow-md p-6">
           {/* Usuario */}
           <div className="mb-4">
             <label className="block text-sm text-gray-700 mb-1">
@@ -62,8 +50,8 @@ export const Login = () => {
               {...register("usuario", {
                 required: "El usuario es obligatorio",
                 minLength: {
-                  value: 4,
-                  message: "Mínimo 4 caracteres",
+                  value: 8,
+                  message: "Mínimo 8 caracteres",
                 },
               })}
             />
@@ -73,7 +61,6 @@ export const Login = () => {
                 {errors.usuario.message as string}
               </p>
             )}
-            />
           </div>
 
           {/* Contraseña */}
@@ -109,25 +96,12 @@ export const Login = () => {
             Acceso Seguro
           </button>
         </form>
-            <input
-              type="password"
-              className="w-full border border-gray-300 rounded px-3 py-2 text-sm"
-            />
-          </div>
-
-          <button 
-          onClick={() => navigate("/verificacion")}
-          className="w-full border border-gray-400 rounded py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 transition">
-            Acceso Seguro
-          </button>
-        </div>
 
         <p className="text-xs text-gray-400 mt-6">
           Protegido con encriptación de 256 bits
         </p>
       </div>
 
-      {/* Footer real */}
       <Footer />
     </div>
   );
